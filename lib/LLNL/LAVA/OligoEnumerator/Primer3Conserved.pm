9876da176ccdd9a4152250a7a6236c4610dd5540
Cheikht4 <cheikhtalibouya@MacBook-Pro-de-Cheikh.local>
2026-07-23 22:58:25 +0000

    Fix préexistant: Exclusion des zones à forte entropie (SEQUENCE_INTERNAL_EXCLUDED_REGION)

--- lib/LLNL/LAVA/OligoEnumerator/Primer3Conserved.pm
+++ lib/LLNL/LAVA/OligoEnumerator/Primer3Conserved.pm
@@ -128,7 +128,7 @@ sub new
     "salt_corrections" => "PRIMER_SALT_CORRECTIONS",
     "thermodynamic_alignment" => "PRIMER_THERMODYNAMIC_ALIGNMENT",
     "thermodynamic_path" => "PRIMER_THERMODYNAMIC_PARAMETERS_PATH",
-    "excluded_regions" => "EXCLUDED_REGION",
+    "excluded_regions" => "SEQUENCE_INTERNAL_EXCLUDED_REGION",
     "included_region" => "INCLUDED_REGION",
   };
 
@@ -367,7 +367,7 @@ sub getOligos
       foreach my $reg (@excludedRegions) {
           push(@regionsStrs, $reg->[0] . "," . $reg->[1]);
       }
-      $primer3->add_targets("EXCLUDED_REGION" => join(" ", @regionsStrs));
+      $primer3->add_targets("SEQUENCE_INTERNAL_EXCLUDED_REGION" => join(" ", @regionsStrs));
   }
 
   my $primer3Results_r = $primer3->run();
